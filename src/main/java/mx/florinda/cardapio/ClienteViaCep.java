3cb1a206c3d540938f448362b19a71749de85104
Uislan Cruz <uislansk8@hotmail.com>
2026-03-23 21:06:17 -0300

    URL

--- src/main/java/mx/florinda/cardapio/ClienteViaCep.java
+++ src/main/java/mx/florinda/cardapio/ClienteViaCep.java
@@ -1,22 +1,29 @@
 package mx.florinda.cardapio;
 
-import java.io.InputStream;
+import java.net.URI;
 import java.net.URL;
-import java.net.URLConnection;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Scanner;
 
 public class ClienteViaCep {
     static void main(String[] args) throws Exception {
 
-        URL url = new URL("https://viacep.com.br/ws/01001000/json/");
-        URLConnection urlConnection = url.openConnection();
+        //URL url = new URL("https://viacep.com.br/ws/01001000/json/");
 
-        InputStream inputStream = urlConnection.getInputStream();
-        Scanner scanner = new Scanner(inputStream);
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            System.out.println(line);
+        URI uri = URI.create("https://viacep.com.br/ws/01001000/json/");
+
+        try
+            (HttpClient httpClient = HttpClient.newHttpClient()) {
+                HttpRequest httpRequest = HttpRequest.newBuilder(uri).build();
+            HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+
+            int statusCode = httpResponse.statusCode();
+            String body = httpResponse.body();
+            System.out.println(statusCode);
+            System.out.println(body);
+
+            }
         }
-
     }
-}
